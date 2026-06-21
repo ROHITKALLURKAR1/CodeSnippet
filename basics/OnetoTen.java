ba40c6479ea298cd56609dba5930cf1b3d0ddcb0
Tukaram Kallurkar <96327207+ROHITKALLURKAR1@users.noreply.github.com>
2026-06-21 23:16:49 +0530

    Add program to print numbers 1 to 10 using infinite loop
    Implemented an infinite loop to print numbers from 1 to 10.

--- basics/OnetoTen.java
+++ basics/OnetoTen.java
@@ -1,1 +1,13 @@
+/* write a program to print 1 to 10 using infinity loop */
+
+public class OneToTen {
+    public static void main(String[] args) {
+        int x=1;
+        for( ; ; ){
+                System.out.println(x);
+                x++;
+                if(x>10) break;
+        }
+    }
+}
 
