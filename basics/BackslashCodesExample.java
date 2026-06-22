5eae1520789089fd68336f058fb23b97f103a4b9
Tukaram Kallurkar <96327207+ROHITKALLURKAR1@users.noreply.github.com>
2026-06-22 20:18:24 +0530

    Add BackslashCodesExample.java to demonstrate escape codes

---
+++ basics/BackslashCodesExample.java
@@ -0,0 +1,51 @@
+public class BackslashCodesExample {
+
+    public static void main(String[] args) {
+
+        // \n - New line
+        System.out.println("\\n - New Line");
+        System.out.println("Hello\nWorld");
+
+        System.out.println();
+
+        // \t - Tab
+        System.out.println("\\t - Tab");
+        System.out.println("Name\tAge");
+
+        System.out.println();
+
+        // \" - Double quote
+        System.out.println("\\\" - Double Quote");
+        System.out.println("He said \"Hello\"");
+
+        System.out.println();
+
+        // \' - Single quote
+        System.out.println("\\' - Single Quote");
+        System.out.println("Character: \'A\'");
+
+        System.out.println();
+
+        // \\ - Backslash
+        System.out.println("\\\\ - Backslash");
+        System.out.println("C:\\Users\\Rohit");
+
+        System.out.println();
+
+        // \r - Carriage return
+        System.out.println("\\r - Carriage Return");
+        System.out.println("12345\rABC");
+
+        System.out.println();
+
+        // \b - Backspace
+        System.out.println("\\b - Backspace");
+        System.out.println("Helloo\b");
+
+        System.out.println();
+
+        // \f - Form feed
+        System.out.println("\\f - Form Feed");
+        System.out.println("Hello\fWorld");
+    }
+}
